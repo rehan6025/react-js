a11abd3a037a7b44922c8d453c5f48fbb7f17f08
rehan6025 <rehan6025@gmail.com>
2025-01-27 12:38:33 +0530

    finished , will fix ui later

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import { InputBox } from './components'
+import {InputBox} from './components'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
-import './App.css'
 
 
 function App() {
+
   const [amount, setAmount] = useState(0)
-  const [from , setFrom] = useState(0)
-  const [to, setTo] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
   const [convertedAmount, setConvertedAmount] = useState(0)
 
-  const currencyInfo = useCurrencyInfo(from);
+  const currencyInfo = useCurrencyInfo(from)
 
   const options = Object.keys(currencyInfo)
 
@@ -20,7 +20,7 @@ function App() {
     setConvertedAmount(amount)
     setAmount(convertedAmount)
   }
-
+  
   const convert = () => {
     setConvertedAmount(amount * currencyInfo[to])
   }
@@ -29,7 +29,7 @@ function App() {
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
-            backgroundImage: `url('https://images.pexels.com/photos/3531895/pexels-photo-3531895.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
+            backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
         }}
     >
         <div className="w-full">
@@ -38,6 +38,7 @@ function App() {
                     onSubmit={(e) => {
                         e.preventDefault();
                         convert()
+                       
                     }}
                 >
                     <div className="w-full mb-1">
@@ -47,6 +48,7 @@ function App() {
                             currencyOptions={options}
                             onCurrencyChange={(currency) => setAmount(amount)}
                             selectCurrency={from}
+                            onAmountChange={(amount) => setAmount(amount)}
                         />
                     </div>
                     <div className="relative w-full h-0.5">
@@ -69,14 +71,13 @@ function App() {
                         />
                     </div>
                     <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
-                        Convert {from.toUpperCase()} to {to.toUpperCase()}
+                        Convert 
                     </button>
                 </form>
             </div>
         </div>
     </div>
-  );
-
+);
 }
 
 export default App
