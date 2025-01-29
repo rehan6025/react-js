5f0d4a365ba5befeceb84e2fc8adb7cc10ce9fdb
rehan6025 <rehan6025@gmail.com>
2025-01-29 21:49:14 +0530

    learned basics of routing and started a project

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+
+import Header from './components/header/header'
+import Home from './components/home/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
