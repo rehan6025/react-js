48d2a4917259d269223131d7960f438a5c240117
rehan6025 <rehan6025@gmail.com>
2025-02-14 23:30:45 +0530

    started blog project and learned how to use backend as a service 🫡

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);    
+  console.log(import.meta.env.VITE_APPWRITE_PRODUCT_ID);    
+
+  
+
+  return (
+    <>
+      <h1>Working</h1>
+    </>
+  )
+
+}
+
+export default App
